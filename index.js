3ceaa7ed7abfb750cf4fc6bb6602b7124bb01cc2
aseelflihan <aseelflihan@gmail.com>
2023-01-24 16:49:41 +0300

    complated assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "Omer is the champion";
+
+document.body.append(newHeader);
